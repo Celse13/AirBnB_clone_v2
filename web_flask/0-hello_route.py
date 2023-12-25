534c4070e24e578722fc69653ef297529f83388b
Celse13 <117368635+Celse13@users.noreply.github.com>
2023-12-25 19:46:33 +0200

    Update Running Web_flask on port 5000

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,12 +8,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello():
     """Displaying "Hello HBNB!" on the webpage"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.url_map.strict_slashes = False
     app.run(host="0.0.0.0", port="5000")
