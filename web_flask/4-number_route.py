feb71fb48a0c90ab3aaf3a680c4a543c77b03073
Celse13 <rugiracelse13@gmail.com>
2023-12-25 11:23:21 -0800

    Validating a variable for routing in my Web Flask App

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Running the flask for my web application
+    listening on 0.0.0.0, port 5000
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Displaying "Hello HBNB!" on the webpage"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display "hbnb" on the webpage"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Displaying c is fun"""
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """Displaying Python is coo"""
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>')
+def display_number(n):
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
