40d8afcd3a99b8b3ca42a626128401cffb3a8670
Celse13 <rugiracelse13@gmail.com>
2023-12-25 10:27:34 -0800

    Routing Using Variable in my Web Flask Application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Running the flask for my web application
+    listening on 0.0.0.0, port 5000
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Displaying "Hello HBNB!" on the webpage"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display "hbnb" on the webpage"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    return 'C {}'.format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
