454871e3f9c73e5ab3fe8486fdb097ca6c606a95
Celse13 <rugiracelse13@gmail.com>
2023-12-26 13:27:55 -0800

    Adding functionality over var parm during routing

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""Running the flask for my web application
+    listening on 0.0.0.0, port 5000
+"""
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Displaying "Hello HBNB!" on the webpage"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display "hbnb" on the webpage"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Displaying c is fun"""
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """Displaying Python is coo"""
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_number_by_template(n):
+    return render_template('5-number.html', value=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def display_number_if_isodd_oreven(n):
+    """Return if the n is even or odd."""
+    if n % 2 == 0:
+        temp = "even"
+    else:
+        temp = 'odd'
+
+    return render_template('6-number_odd_or_even.html', value=n, text=temp)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
