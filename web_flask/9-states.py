5866a9c2c12841dc442b1445e3a29a8872ee9a77
Celse13 <117368635+Celse13@users.noreply.github.com>
2023-12-31 19:31:15 +0200

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""Running the flask for my web application
-    listening on 0.0.0.0, port 5000
-"""
-from flask import Flask
-from flask import render_template
+"""Importing Flask to run the web app"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 
@@ -11,30 +8,27 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def the_states(id=None):
-    """List states"""
-    states = retrieve_state_by_id(id)
-    return render_template('9-states.html', states=states, state_id=id)
-
-
-def retrieve_state_by_id(state_id):
-    """Retrieve state by state_id"""
-    states = storage.all(State)
-    if state_id:
-        key = "{}.{}".format(State.__name__, state_id)
-        states = states.get(key, None)
-    else:
-        states = states.values()
-    return states
-
-
 @app.teardown_appcontext
-def close(arg=None):
-    """Close session when app conten torn down."""
+def close(self):
+    """ Method to close the session """
     storage.close()
 
 
-if __name__ == "__main__":
+@app.route('/states', strict_slashes=False)
+def state():
+    """Displays a html page with states"""
+    states = storage.all(State)
+    return render_template('9-states.html', states=states, mode='all')
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_by_id(id):
+    """Displays a html page with citys of that state"""
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template('9-states.html', states=state, mode='id')
+    return render_template('9-states.html', states=state, mode='none')
+
+
+if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
