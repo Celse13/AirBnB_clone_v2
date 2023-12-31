c4b086d205d6d5b52d67656042d6470af5ad537f
Celse13 <117368635+Celse13@users.noreply.github.com>
2023-12-31 19:20:34 +0200

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,36 +1,40 @@
 #!/usr/bin/python3
-"""List cities by states"""
-from flask import Flask, render_template
+"""Running the flask for my web application
+    listening on 0.0.0.0, port 5000
+"""
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
 
+
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def teardown():
-    """tear down"""
-    storage.close()
 
 
 @app.route('/states', strict_slashes=False)
 @app.route('/states/<id>', strict_slashes=False)
 def the_states(id=None):
     """List states"""
-    states = get_states_by_id(id)
-    return render_template('9-states.html', states=states, id=id)
+    states = retrieve_state_by_id(id)
+    return render_template('9-states.html', states=states, state_id=id)
 
 
-def get_states_by_id(id):
-    """get states by id"""
+def retrieve_state_by_id(state_id):
+    """Retrieve state by state_id"""
     states = storage.all(State)
-    if id:
-        key = f"{State.__name__}.{id}"
+    if state_id:
+        key = "{}.{}".format(State.__name__, state_id)
         states = states.get(key, None)
     else:
         states = states.values()
     return states
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.teardown_appcontext
+def close(arg=None):
+    """Close session when app conten torn down."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
